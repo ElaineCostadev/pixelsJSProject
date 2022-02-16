116a74d6d077895f4f78b0896dabb2ccf2b7e2b3
ElaineCosta <elainerscosta@hotmail.com>
2022-02-16 16:36:18 -0300

    requesito6 manual

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-window.onload = function(){
-
-    //faca aqui
-
-
-
+/* window.onload = function(){
+    let selectBlack = document.getElementById("black");
+    let newBlack = document.createElement("selected");
+    newBlack.className = "selected";
 
 
-}
+    selectBlack.appendChild(newBlack);
+
+    localStorage.setItem("colorBlack", newBlack );
+
+} */
+
+
 
 
 
