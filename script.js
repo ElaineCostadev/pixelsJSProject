d260ccb3773861b6fb80925153021d57f271056b
ElaineCosta <elainerscosta@hotmail.com>
2022-02-23 14:48:52 -0300

    requesito 9

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 /* window.onload = function(){ */
 
    let colorPalette = document.querySelector("#color-palette");
+   const selecionarPixel = document.querySelectorAll(".pixel");
+   let btnClear = document.querySelector("#clear-board");
+
    colorPalette.addEventListener("click", selectedClass);
 
 
- /*   let classColor = document.querySelectorAll(".color");
-   let criateSelected = document.createElement("selected");
-   criateSelected.value = classColor.value; */
-
-   /* classColor.appendChild(criateSelected); */
-   
  
 function selectedClass (event){
    let classSelected = document.getElementsByClassName("selected")[0];
@@ -19,10 +16,11 @@ function selectedClass (event){
 
 //mentorias e https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
 
+
 function mudarCorPixelBoard (){
    
-   const selecionarPixel = document.querySelectorAll(".pixel");
    for(let index = 0; index < selecionarPixel.length; index +=1){
+
       selecionarPixel[index].addEventListener("click", function(event){
          const elementCorAMudar = document.querySelector(".selected");
          let colorSelectedPixel = getComputedStyle(elementCorAMudar).backgroundColor;
@@ -35,11 +33,38 @@ mudarCorPixelBoard();
 
 
 
+
+ function clearAllPixels (){
+
+       btnClear.addEventListener("click", function(){ 
+
+         for(let index = 0; index < selecionarPixel.length; index +=1){
+            selecionarPixel[index].style.backgroundColor = "white";
+         }
+      });
+   }
+   clearAllPixels();
+            
+
+          
+  
+
+        
+       /*    */
+       
+         /* let btnLimpar = document.querySelector("#clear-board"); */
+
+
+
+
+
+
  
 
 
 
 
+  
 
 
 
