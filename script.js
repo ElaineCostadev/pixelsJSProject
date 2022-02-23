aea74e817d64618249808a7e9db9f927802e561e
ElaineCosta <elainerscosta@hotmail.com>
2022-02-22 22:19:03 -0300

    tentando ajustar requesito 6

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-/* window.onload = function(){
-    let selectBlack = document.getElementById("black");
-    let newBlack = document.createElement("selected");
-    newBlack.className = "selected";
+/*  window.onload = function(){ */
+    let selectBlack = document.querySelectorAll(".color");
+    let newSelected = document.createElement("selected");
+    selectBlack.appendChild(newSelected);
+   
+    
+
+    newSelected.addEventListener("click", function(event){
+    let classSelected = document.querySelector(".selected");
+    classSelected.classList.remove("selected");
+    event.target.classList.add("selected"); 
+    
+    if(classSelected !== null ){
+        
+       }
+      
+       
+       console.log(newSelected);
+});
+  /*   newSelected.className = "selected";
+    
+     */
+
+/*     localStorage.setItem("colorBlack", newSelected );
+ */
 
 
-    selectBlack.appendChild(newBlack);
+    
+        
+  
+    
 
-    localStorage.setItem("colorBlack", newBlack );
 
-} */
+
 
 
 
